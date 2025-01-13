84fb2d5be19b385dbd5f66c7f19e76479feb563b
Jorge Benjumea <me@intron014.com>
2025-01-13 14:52:36 +0100

    Initial Commit

---
+++ EMT TimesTests/EMT_TimesTests.swift
@@ -0,0 +1,17 @@
+//
+//  EMT_TimesTests.swift
+//  EMT TimesTests
+//
+//  Created by Jorge Benjumea on 13/1/25.
+//
+
+import Testing
+@testable import EMT_Times
+
+struct EMT_TimesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ EMT TimesUITests/EMT_TimesUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  EMT_TimesUITestsLaunchTests.swift
+//  EMT TimesUITests
+//
+//  Created by Jorge Benjumea on 13/1/25.
+//
+
+import XCTest
+
+final class EMT_TimesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
